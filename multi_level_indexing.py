5814cb9d56f58e5da34fe64746edf32ad9b1aff5
Joseph Ogbu <57672199+fallyfox@users.noreply.github.com>
2019-12-07 12:53:09 +0100

    Create multi_level_indexing.py

---
+++ multi_level_indexing.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+outside = ['G1','G1','G1','G2','G2','G2']
+inside = [1,2,3,1,2,3]
+hier_index = list(zip(outside,inside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
