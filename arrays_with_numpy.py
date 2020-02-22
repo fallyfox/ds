ea30a1af56eb11227510780d034ac145dd9e85dc
Joseph Ogbu <57672199+fallyfox@users.noreply.github.com>
2020-02-22 20:18:00 +0100

    Create arrays_with_numpy.py

---
+++ arrays_with_numpy.py
@@ -0,0 +1,9 @@
+#ASSIGNMENT
+
+#Generate random integers between 1 - 5000
+#Generatea total 100 values
+#Get min and max values
+#Find standard deviation among those set of randomly generated value
+
+#Find out many possible values we can get between the range involving the min and std with a step size of 3
+#Find out many possible values we can get between the range involving the max and std with a step size of 3
